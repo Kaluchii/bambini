5a14b47e14310f386652019c0244f8902e58f51f
Konstantin <mr.kaluchii@mail.ru>
2016-05-23 11:23:01 +0600

    fidback

--- config/fidback.php
+++ config/fidback.php
@@ -2,7 +2,7 @@
 
 return [
     'block_name' => 'fidback',
-    'mail_username' => 'mr.kaluchii@mail.ru',
+    'mail_username' => 'aaron.klark@mail.ru',
     'site_name' => 'Bambini',
     'mail_rec' => 'demigod96@bk.ru',
     'mail_rec_copy1' => 'mr.kaluchii@mail.ru',
