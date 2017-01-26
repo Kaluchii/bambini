d1a44790b149e8878e76e5c32d2de53f1011811c
Kaluchii <mr.kaluchii@mail.ru>
2017-01-26 18:52:05 +0600

    commet

---
+++ resources/views/front/footer.blade.php
@@ -0,0 +1,21 @@
+@section('footer')
+    <footer class="footer">
+        <div class="row">
+            <div class="wrap">
+                <div class="col-1-2">
+                    <p>{{$all_site->copy_field}}</p>
+                </div>
+                <div class="col-1-2">
+                    <p>Сделано в <a href="http://interpro.kz">ИнтерПро</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="wrap">
+                <div class="col-1-2">
+                    <p>Мы в <a href="https://www.instagram.com/bambini.club/">Инстаграме</a>, <a href="https://vk.com/club82363318">ВКонтакте</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+@endsection
